d61f1e03426e3982b65b5de1b8305212eea9fe88
Билигма <z.bilyashik@gmail.com>
2024-02-22 00:10:58 +0900

    +++

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class Game:
         self.level.run()
 
 
-levels = {0: 'design/image.png', 1: 'design/природаа.jpg'}
+levels = {0: 'design/image.png', 1: 'design/природаа.jpg', 2: 'design/roof.png'}
 
 
 def main(a):
